66825ba23863bb6fd9c5baf0feca2ad1bb6dc50c
root <root@ubuntu.(none)>
2014-09-16 15:34:52 -0600

    Implemented

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -35,26 +36,55 @@ vector<string>& splitstring::split(char delim, int rep) {
     return flds;
 }
 
+int countTargets(const char *line, const char *target)
+{
+    int count = 0;
+    char *lin= (char*)line;
+    int tsize = strlen(target);
+    //int size2 = strlen(*target);
+    char *p = NULL;
+    while (p = strstr(lin, target))
+    {
+        lin = p + tsize;
+        cout << p << std::endl;
+        count++;
+    }
+    return count;
+}
+
 int main(int ArgC, char *ArgV[])
 {
-    int argC = ArgC;
     char *ArgV1 = ArgV[2];
-    char *argV2 = ArgV[3];
-    cout << ArgV1<< endl;
+
+    cout << "Word to search: "<< ArgV1 << endl;
+    vector<string> vec_lines;
+    // Count search 1
     int count = 0;
-    for (string line; std::getline(std::cin, line);) {
-        // splits in the vector of strings
-        const char* p = line.c_str();
-        char* q = const_cast<char*>(p);
-        splitstring str_line = q;
-        vector<string> vec_line = str_line.split(' ');
-        for (int k = 0; k < vec_line.size(); k++)
-        {
-            if(vec_line[k] == ArgV1)
-                count++;
+        for (string line; std::getline(std::cin, line);) {
+            vec_lines.push_back(line);
+            // splits in the vector of strings
+            const char* p = line.c_str();
+            char* q = const_cast<char*>(p);
+            splitstring str_line = q;
+            vector<string> vec_line = str_line.split(' ');
+            for (int k = 0; k < vec_line.size(); k++){
+                if(vec_line[k] == ArgV1)
+                    count++;
+            }
         }
+        std::cout << "(1) Words count: " << count << std::endl;
+
+    // Count search 1
+    count = 0;
+    //vec_lines.push_back("This a NULL word of NULL that live in NULL-NUL!!");
+
+    for (int i = 0; i < vec_lines.size(); i++) {
+        //cout << "(2) Line text: "<< vec_lines[i] << std::endl;
+        count += countTargets(vec_lines[i].c_str(), ArgV1);
     }
-    std::cout << " Words count: " << count << std::endl;
+    std::cout << "(2) Words count: " << count << std::endl;
+
+
     return 0;
     // Counter -t "text" < ../kiuti/FindMe.txt
 }
