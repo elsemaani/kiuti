2650643410146156ec139e318d22d31b43295f7c
alex <alex@ubuntu.(none)>
2014-09-16 10:48:48 -0600

    Added cin

--- main.cpp
+++ main.cpp
@@ -6,8 +6,15 @@ int main(int ArgC, char *ArgV[])
 {
     int argC = ArgC;
     char *ArgV1 = ArgV[2];
+    char *argV2 = ArgV[3];
     cout << ArgV1;
-    cout << " - Hello World!" << endl;
+    int count = 0;
+    for (string line; std::getline(std::cin, line);) {
+            std::cout << line << std::endl;
+            count++;
+        }
+    std::cout << " Words count: " << count << std::endl;
     return 0;
+    // Counter -t "text" < ../kiuti/FindMe.txt
 }
 
